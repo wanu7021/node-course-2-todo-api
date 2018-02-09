4f5fb0f57eeb3f5b52da8b6c859969ef1fb62f79
Manuel Iglesias <miglesias@salesforce.com>
2018-02-09 06:44:23 -0800

    add Post /todos and users routes and refactor

---
+++ server/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo(req.body);
+  todo.save().then((doc) => {
+    res.send(doc);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.post('/users', (req, res) => {
+  const user = new User(req.body);
+  user.save().then((doc) => {
+    res.send(doc);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
