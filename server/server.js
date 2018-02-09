d963c2ff071ace1620b21de93efb2136d4b570d7
Manuel Iglesias <miglesias@salesforce.com>
2018-02-09 11:38:28 -0800

    fix function name for delete

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.delete('/todos/:id', (req, res) => {
   if (!ObjectID.isValid(id)) {
     return res.status(404).send({});
   } 
-  Todo.findByIdAndDelete(id).then( todoRemoved => {
+  Todo.findByIdAndRemove(id).then( todoRemoved => {
     if (todoRemoved) {
       res.send({todoRemoved});
     } else {
