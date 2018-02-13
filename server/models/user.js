6152409a624a0648cca6be349c8db152a8f2dcd0
Manuel Iglesias <miglesias@salesforce.com>
2018-02-13 07:11:56 -0800

    add post users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,56 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
   },
   password: {
     type: String,
     required: true,
-    minlength: 1,
+    minlength: 6,
     trim: true
-  }
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
+
+UserSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  const user = this;
+  const access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens = user.tokens.concat([{access, token}])
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -98,10 +98,14 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-  const user = new User(req.body);
-  user.save().then((doc) => {
-    res.send(doc);
-  }).catch((e) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then( token => {
+    res.header('x-auth', token).send(user);
+  })
+  .catch((e) => {
     res.status(400).send(e);
   });
 });
